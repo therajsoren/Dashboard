620e5fb9454171d705fe606de44122a340679543
Raj Soren <7rajkumarsoren@gmail.com>
2025-08-10 20:25:16 +0530

    init project, add sidebar, navbar

---
+++ components/ui/Navbar.tsx
@@ -0,0 +1,58 @@
+"use client";
+import { LogOut, Moon, Settings, Sun, User } from "lucide-react";
+import Link from "next/link";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useTheme } from "next-themes";
+const Navbar = () => {
+  const { theme, setTheme } = useTheme();
+  return (
+    <nav className="p-4 flex items-center justify-between">
+      dcol
+      <div className="flex items-center gap-4">
+        <Link href="/">DashBoard</Link>
+        {theme === "dark" ? (
+          <button className="cursor-pointer" onClick={() => setTheme("light")}>
+            <Sun />
+          </button>
+        ) : (
+          <button className="cursor-pointer" onClick={() => setTheme("dark")}>
+            <Moon />
+          </button>
+        )}
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Avatar>
+              <AvatarImage src="https://github.com/shadcn.png" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent sideOffset={10}>
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <User />
+              Profile
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <Settings />
+              Settings
+            </DropdownMenuItem>
+            <DropdownMenuItem variant="destructive">
+              <LogOut />
+              Logout
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </nav>
+  );
+};
+export default Navbar;
