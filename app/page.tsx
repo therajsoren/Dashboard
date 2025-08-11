6cc71484b8b96099ab9115aeaa81e4bf8d14b656
Raj Soren <7rajkumarsoren@gmail.com>
2025-08-11 19:26:06 +0530

    add todolist, cardlist, appbarchart, appareachart apppiechart components

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,30 @@
-
+import AppAreaChart from "@/components/AppAreaChart";
+import AppBarChart from "@/components/AppBarChart";
+import AppPieChart from "@/components/AppPieChar";
+import CardList from "@/components/CardList";
+import TodoList from "@/components/TodoList";
 
 export default function Home() {
   return (
-    <div>
-      
+    <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
+      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
+        <AppBarChart />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg">
+        <CardList title="Latest Transactions" />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg">
+        <AppPieChart />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg">
+        <TodoList />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
+        <AppAreaChart />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg">
+        <CardList title="Popular Content" />
+      </div>
     </div>
   );
 }
